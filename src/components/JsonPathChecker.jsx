296999746587058d1de0638fc4e2c957a92a3850
Andy Chang <andy.chang@tourismmedia.com.au>
2024-09-10 18:55:08 +1000

    style: Updated styling and content alignment

--- src/components/JsonPathChecker.jsx
+++ src/components/JsonPathChecker.jsx
@@ -1,13 +1,15 @@
 export function JsonPathChecker() {
     return (
-        <section className="json-path-checker">
-            <div className="path">
-                <label className="path_label">JSON Path </label>
-                <input className="path_input"/>
-            </div>
-            <div className="results">
-                <label className="results_label">Evaluation Results</label>
-                <input className="results_output"/>
+        <section className="path-checker">
+            <div className="path-checker__container">
+                <div className="path">
+                    <label className="path_label">JSON Path </label>
+                    <input className="path_input"/>
+                </div>
+                <div className="results">
+                    <label className="results_label">Evaluation Results</label>
+                    <input className="results_output"/>
+                </div>
             </div>
         </section>
     )
--- src/components/JsonSchemaGenerator.jsx
+++ src/components/JsonSchemaGenerator.jsx
@@ -1,15 +1,17 @@
 export function JsonSchemaGenerator() {
     return (
         <section className="schema-generator">
-            <div className="json_input_box">
-                <label className="json_label">JSON Input </label>
-                <input className="json_input"/>
-                <button className="generate_btn">Generate Schema</button>
-            </div>
-            <div className="json_schema">
-                <label className="schema_label">JSON Schema</label>
-                <input className="schema_output"/>
-                <div className="error-box">Error message</div>
+            <div className="schema-generator__container">
+                <div className="json_input_box">
+                    <label className="json_label">JSON Input </label>
+                    <input className="json_input"/>
+                    <button className="generate_btn">Generate Schema</button>
+                </div>
+                <div className="json_schema">
+                    <label className="schema_label">JSON Schema</label>
+                    <input className="schema_output"/>
+                    <div className="error-box">Error message</div>
+                </div>
             </div>
         </section>
     )
